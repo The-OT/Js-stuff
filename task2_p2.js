de2d0b14ef9c4557d8bbf012e7c2698d84a55683
0S-Car <98452450+0S-Car@users.noreply.github.com>
2022-07-08 20:57:46 +0545

    Task 2 part 2

---
+++ task2_p2.js
@@ -0,0 +1,9 @@
+const person = {
+  name: prompt("Name?"),
+  rollNo: prompt("Roll No.?"),
+}
+
+console.log(person.name)
+console.log(person.rollNo)
+document.write(`Name: ${person.name}`)
+document.write(`Roll no.: ${person.rollNo}`)
